d4e3425eea2ca48c5e70adb579305ec4ee006463
Khangaii <Khangaii2005@gmail.com>
2023-04-29 10:31:09 +0900

    feat: Switching between modes in AppMenu

--- FidgetApp/Windows/AppControlWindow.xaml.cs
+++ FidgetApp/Windows/AppControlWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -16,15 +17,18 @@ namespace FidgetApp.Windows
 {
     public partial class AppControlWindow : Window
     {
-        private PhysicsBallWindow physicsBallWindow;
+        private Window currentWindow;
 
         public AppControlWindow()
         {
             InitializeComponent();
 
-            physicsBallWindow = new PhysicsBallWindow();
+            currentWindow = new PhysicsBallWindow
+            {
+                Tag = "0"
+            };
 
-            physicsBallWindow.Show();
+            currentWindow.Show();
         }
 
         private void Exit(object sender, RoutedEventArgs e)
@@ -49,6 +53,45 @@ namespace FidgetApp.Windows
                 {
                     AppMenu.RowDefinitions[i].Height = GridLength.Auto;
                 }
+            }
+        }
+
+        private void AppMode_Checked(object sender, RoutedEventArgs e)
+        {
+            ToggleButton toggleButton = sender as ToggleButton;
+            string previousTag = currentWindow.Tag.ToString();
+            string currentTag = toggleButton.Tag.ToString();
+
+            if (previousTag != currentTag)
+            {
+                ToggleButton previousToggleButton = AppModes.Children[Convert.ToInt32(previousTag)] as ToggleButton;
+
+                currentWindow.Close();
+
+                switch (currentTag)
+                {
+                    case "0":
+                        currentWindow = new PhysicsBallWindow();
+                        break;
+                    case "1":
+                        currentWindow = new AimTrainerWindow();
+                        break;
+                }
+                currentWindow.Tag = currentTag;
+
+                // Uncheck previous app mode toggle button
+                previousToggleButton.IsChecked = false;
+
+                currentWindow.Show();
+            }
+        }
+
+        private void AppMode_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ToggleButton toggleButton = sender as ToggleButton;
+            if (currentWindow.Tag.ToString() == toggleButton.Tag.ToString())
+            {
+                toggleButton.IsChecked = true;
             }
         }
     }
